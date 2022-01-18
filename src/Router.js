4375d0a2ea41b8efdecf67f74d83f04e21237319
woongstaa <jinung91@gmail.com>
2022-01-18 14:24:20 +0900

    Add: Top, Nav, Footer and SNKRS List

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Top from './components/Top';
+import TopNav from './components/TopNav';
 import Detail from './pages/Detail/Detail';
 import SNKRSDetail from './pages/SNKRSDetail/SNKRSDetail';
+import SnkrsList from './pages/SNKRS/SnkrsList';
 
 function Router() {
   return (
@@ -8,6 +12,10 @@ function Router() {
       <Routes>
         <Route path="/detail" element={<Detail />} />
         <Route path="/snkrsdetail" element={<SNKRSDetail />} />
+        {/* <Route path="/" element={<Top />} /> */}
+        <Route path="/" element={<TopNav />} />
+        {/* <Route path="/" element={<Footer />} /> */}
+        <Route path="/snkrs" element={<SnkrsList />} />
       </Routes>
     </BrowserRouter>
   );
